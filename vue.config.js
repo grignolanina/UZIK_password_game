7b45fcca024a54e409a8412dc3d230751f1c2bbb
grignolanina <grignola.nina@orange.fr>
2024-07-26 15:40:27 +0200

    Ajout de la configuration pour le déploiement sur Netlify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: '/', // Définir le chemin de base pour le déploiement
+  configureWebpack: {
+    // Optionnel : Configurations supplémentaires si nécessaire
+  },
+  devServer: {
+    // Optionnel : Configurations du serveur de développement
+  }
+});
